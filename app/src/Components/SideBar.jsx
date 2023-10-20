77aae28b8ab11926082c0c793e87a57294c3f03f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-20 21:07:39 +0100

    side bar

---
+++ app/src/Components/SideBar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+const SideBar = () => {
+    return (
+        <div className='fixed pr-14 bg-green-100 h-screen flex flex-col items-start justify-start py-10 gap-2'>
+            <Link to={"/"} className='font-medium hover:text-green-800 rounded-md
+             hover:bg-green-300 hover:font-semibold w-full py-1 px-3'>
+                Home
+
+            </Link>
+            <Link to={"/archive"} className='font-medium hover:text-green-800 rounded-md
+             hover:bg-green-300 hover:font-semibold w-full py-1 px-3'>
+                Archive
+            </Link>
+            <Link to={"/create"} className='font-medium hover:text-green-800 rounded-md
+             hover:bg-green-300 hover:font-semibold w-full py-1 px-3'>
+                New Post
+            </Link>
+        </div>
+    )
+}
+
+export default SideBar
