223fa0c93477c4ae5ef0e71dd74e6c2df05a6587
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-20 21:08:28 +0100

    archive page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
 import Archive from './Pages/Archive'
 import Home from './Pages/Home'
+import Create from './Pages/Create'
+import Post from './Pages/Post'
+import SideBar from './Components/SideBar'
 
 function App() {
 
   return (
-    <>
-      {/* <Home /> */}
+    <div className='flex font-josefin justify-center mx-8 '>
+      <div className='flex-[1] w-full h-screen bg-green-100 '>
+        <SideBar />
+      </div>
+      <div className='flex-[9]'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/archive" element={<Archive />} />
+          <Route path="/create" element={<Create />} />
+          <Route path="/allposts/posts" element={<Post />} />
+        </Routes>
+      </div>
 
-      <Archive/>
-    </>
+    </div>
   )
 }
 
--- app/src/Pages/Archive.jsx
+++ app/src/Pages/Archive.jsx
@@ -1,17 +1,17 @@
 import axios from "axios"
 import { useEffect, useState } from "react"
-import { baseURL } from "../constants/constants"
+import { baseURL, capitalized, createDate } from "../constants/constants"
+import { Link } from "react-router-dom"
+import { HashLoader} from "react-spinners"
 
 
 const Archive = () => {
     const [posts, setPosts] = useState()
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
-        setLoading(true)
         axios.get(`${baseURL}posts/`)
             .then(res => {
-
                 console.log(res.data)
                 setPosts(res.data)
                 setLoading(false)
@@ -19,20 +19,37 @@ const Archive = () => {
     }, [])
 
     return (
-        <div className=" flex flex-col items-center justify-center gap-3">
+        <div className=" h-screen flex flex-col px-10 py-5">
 
-            <h1>Archive</h1>
+            <h1 className="text-4xl text-green-800 font-semibold">All Posts</h1>
 
             {
-                loading ? (<p className=" text-5xl animate-spin w-fit flex items-center justify-center">X</p>) : (
+                loading ? (<div className=" flex flex-col items-center m-auto justify-center ">
+                     <HashLoader color="#36d7b7" /> 
+                     </div>) : (
 
-                    <div className="flex flex-col items-center justify-center gap-10">
-                        {posts?.map(items => (
-                            <div className=" flex flex-col items-center justify-center px-20 gap-2" key={items._id}>
-                                <p>{items.title}</p>
-                                <p>{items.author}</p>
-                            </div>
-                        ))}
+                    <div className="flex flex-col gap-10 mt-2 p-10">
+                        {
+                            posts?.map(data => (
+                                <div className="w-full flex flex-col border-2 border-black rounded-3xl p-3 gap-5" key={data._id}>
+                                    <h1 className="text-3xl text-green-800 font-bold">{data.title}</h1>
+                                    <div className="flex flex-col gap-1">
+                                        <p className="font-semibold">
+                                            by {capitalized(data.author)} on {createDate(data.date)}
+                                        </p>
+                                        <Link
+                                            to={"/"} className="text-blue-800 underline font-semibold">
+                                            Read more...
+                                        </Link>
+                                    </div>
+                                    <div className="flex gap-1">
+                                        {data.tags.map((tag, index) => (
+                                            <span className="bg-slate-600 text-white rounded-xl py-1 px-3 items-center" key={index}>{tag}</span>
+                                        ))}
+                                    </div>
+                                </div>
+                            ))
+                        }
                     </div>
                 )
             }
