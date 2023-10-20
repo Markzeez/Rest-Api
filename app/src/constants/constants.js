50acd72ac6929f7a8f42fc3a6e4516cacaa74976
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-20 21:08:11 +0100

    edits

--- app/src/constants/constants.js
+++ app/src/constants/constants.js
@@ -1,1 +1,13 @@
-export const baseURL = "http://localhost:3003/"
+export const baseURL = "http://localhost:3003/";
+
+export const capitalized = (word) => {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+export const createDate = (date) => {
+  let yy = date.slice(0, 4);
+  let dd = date.slice(5, 7);
+  let mm = date.slice(8, 10);
+
+  return `${dd}/${yy}/${mm}`;
+};
--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily:{
+      josefin : ["Josefin Sans"]
+    }
   },
   plugins: [],
 }
