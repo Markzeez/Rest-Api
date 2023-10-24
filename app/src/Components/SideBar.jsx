a097a5326ec8e7b5fdd53277a06028713073319f
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2023-10-24 14:43:31 +0100

    form component

--- app/src/Components/SideBar.jsx
+++ app/src/Components/SideBar.jsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom'
 
 const SideBar = () => {
     return (
-        <div className='fixed pr-14 bg-green-100 h-screen flex flex-col items-start justify-start py-10 gap-2'>
+        <div className='fixed left-0 pr-14 bg-green-100 h-screen flex flex-col items-start justify-start py-10 gap-2'>
+            <img src='' />
             <Link to={"/"} className='font-medium hover:text-green-800 rounded-md
              hover:bg-green-300 hover:font-semibold w-full py-1 px-3'>
                 Home
--- app/src/Pages/Create.jsx
+++ app/src/Pages/Create.jsx
@@ -1,8 +1,12 @@
+import Form from "../Components/Form"
 
 
 const Create = () => {
   return (
-    <div>Create</div>
+    <div className="px-10 py-10 ">
+      <h1 className="text-4xl text-green-800 font-semibold">Create</h1>
+      <Form />
+    </div>
   )
 }
 
